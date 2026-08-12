e80900bc77e555d9562527dc487409fa28420021
piyush singh tanwar <piyushsinghtomar777@gmail.com>
2026-08-13 01:47:25 +0530

    feat: initialize socket.io client configuration for websocket communication

--- src/webservices/webSocket/socket.js
+++ src/webservices/webSocket/socket.js
@@ -1,3 +1,5 @@
+import { io } from "socket.io-client";
+
 const backendUrl = import.meta.env.VITE_ENDPOINT_URL || "https://chatapplication-backend-ddk8.onrender.com";
 
 export const socket = io(backendUrl, {
